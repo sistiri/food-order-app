f918c7ed1da01077433ce5794926725924a69248
sistiri <sistiri@yahoo.com>
2022-02-08 10:44:15 +0100

    Adding a Form & an Input UI Components

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,8 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
-
-    const price = `$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +11,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
