1525672ee07bc227186f88be4b4e349a2c75f152
sistiri <sistiri@yahoo.com>
2022-02-14 11:04:14 +0100

    Moving Fetch Logic to AvailableMeals Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState("false");
-  const [meals, setMeals] = useState([]);
-
-  const apiURL =
-    "https://food-order-app-255b2-default-rtdb.europe-west1.firebasedatabase.app/DUMMY_MEALS.json";
-
-  useEffect(() => {
-    const fetchAvailableMeals = async () => {
-      const response = await fetch(apiURL);
-      const responseData = await response.json();
-      console.log(responseData);
-      const loadedMeals = [];
-
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: responseData[key].id,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        });
-      }
-      setMeals(loadedMeals);
-      console.log(loadedMeals);
-    };
-    fetchAvailableMeals();
-  }, []);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -45,7 +20,7 @@ function App() {
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
-        <Meals meals={meals} />
+        <Meals/>
       </main>
     </CartProvider>
   );
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = (props) => {
-  const mealsList = props.meals.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  const apiURL =
+    "https://food-order-app-255b2-default-rtdb.europe-west1.firebasedatabase.app/DUMMY_MEALS.json";
+
+  useEffect(() => {
+    const fetchAvailableMeals = async () => {
+      const response = await fetch(apiURL);
+      const responseData = await response.json();
+      console.log(responseData);
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: responseData[key].id,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      console.log(loadedMeals);
+    };
+    fetchAvailableMeals();
+  }, [props.url]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
