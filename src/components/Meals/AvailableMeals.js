9dcada2c2ffcc3d298a6b10c52a541e56f82521c
sistiri <sistiri@yahoo.com>
2022-02-14 11:55:12 +0100

    try-catch error handling - not working

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,32 +5,44 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = (props) => {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(false);
 
   const apiURL =
-    "https://food-order-app-255b2-default-rtdb.europe-west1.firebasedatabase.app/DUMMY_MEALS.json";
+    "https://food-order-app-255b2-default-rtdb.europe-west1.firebasedatabase.app/DUMMY_MEALS.json"
 
   useEffect(() => {
-    const fetchAvailableMeals = async () => {
-      const response = await fetch(apiURL);
-      const responseData = await response.json();
-      console.log(responseData);
-      const loadedMeals = [];
+    // try {
+      const fetchAvailableMeals = async () => {
+        setIsLoading(true);
+        const response = await fetch(apiURL);
+        const responseData = await response.json();
+        console.log(responseData);
+        const loadedMeals = [];
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: responseData[key].id,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+        
+        setMeals(loadedMeals);
+        setIsLoading(false);
+        console.log(loadedMeals);
+      };
+      fetchAvailableMeals();
+    // } catch (error) {
+    //   setError(true);
+      // throw error("Error in loading!");
+      // console.log(error)
+    // }
+  }, []);
 
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: responseData[key].id,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        });
-      }
-      setMeals(loadedMeals);
-      setIsLoading(false)
-      console.log(loadedMeals);
-    };
-    fetchAvailableMeals();
-  }, [props.url]);
+  // if (error) {
+  //   return <p className={classes.loading}>Error in loading!</p>;
+  // }
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -44,11 +56,15 @@ const AvailableMeals = (props) => {
 
   return (
     <Fragment>
-      {isLoading ? <p className={classes.loading}>Loading...</p> : <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
-      </section>}
+      {isLoading ? (
+        <p className={classes.loading}>Loading...</p>
+      ) : (
+        <section className={classes.meals}>
+          <Card>
+            <ul>{mealsList}</ul>
+          </Card>
+        </section>
+      )}
     </Fragment>
   );
 };
